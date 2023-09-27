a618095c226f9bfecffdcef85d635e4f533df2f9
uletin <dokterkoding111@gmail.com>
2023-09-27 15:56:00 +0800

    feat pagination

--- src/app/superadmin/carts/page.tsx
+++ src/app/superadmin/carts/page.tsx
@@ -3,24 +3,27 @@
 import H2TextField from "@/components/atoms/textField/H2Text";
 import apiService from "@/service/api/apiService";
 import UrlService from "@/service/url/UrlService";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import CartsCard from "@/components/atoms/cards/CartsCard";
+import Pagination from "@/components/molecules/pagination/Pagination";
 
-const CartLists = async () => {
+const CartLists = async ({ skip }: any) => {
   const api: any = new apiService();
   const url: any = new UrlService();
   const headers = {};
-  const limit = 5
-  const skip = 5
+  const limit = 5;
 
   const { carts } = await api.get(url.endpoint.carts, headers, limit, skip);
 
-  const getUserFullname = async (id:number) => {
-      const {firstName, lastName } = await api.getById(url.endpoint.users, headers, id)
-      const fullname = `${firstName} ${lastName}`
-
-      return fullname
-  }
+  const getUserFullname = async (id: number) => {
+    const { firstName, lastName } = await api.getById(
+      url.endpoint.users,
+      headers,
+      id
+    );
+    const fullname = `${firstName} ${lastName}`;
+    return fullname;
+  };
 
   return (
     <>
@@ -39,16 +42,46 @@ const CartLists = async () => {
   );
 };
 
+const CartPagination = ({ handlePrev, handleNext }: any) => {
+  return (
+    <div className="flex justify-center pt-6">
+      <Pagination prev={handlePrev} next={handleNext} />
+    </div>
+  );
+};
+
 const AdminCartsPage = () => {
+  const [skip, setSkip] = useState(5);
+
+  const handlePrev = () => {
+    setSkip((prev) => prev - 5);
+  };
+
+  const handleNext = () => {
+    setSkip((prev) => prev + 5);
+  };
+
+  console.log(`universal ${skip}`);
+
   return (
     <div className="p-6 ">
       <div className="pb-6">
         <H2TextField text="Carts" alignText="left" />
       </div>
-      <div className="flex flex-wrap gap-2">
+      <div>
         <Suspense fallback={<p>Loading ...</p>}>
-          <CartLists />
+          <div className="flex flex-wrap gap-2 min-h-fit">
+            <CartLists
+              skip={skip}
+              handlePrev={handlePrev}
+              handleNext={handleNext}
+            />
+          </div>
         </Suspense>
+        <CartPagination
+          handleNext={skip <= 0 ? null : handleNext}
+          handlePrev={skip <= 20 ? handlePrev : undefined}
+        />
       </div>
     </div>
   );
---
+++ src/components/molecules/pagination/Pagination.tsx
@@ -0,0 +1,39 @@
+interface IPagination {
+  prev: () => void;
+  next: () => void;
+}
+
+const Pagination = ({ prev, next }: IPagination) => {
+  return (
+    <nav aria-label="Page navigation example">
+      <ul className="inline-flex -space-x-px text-sm">
+        <li>
+          <a
+            onClick={prev}
+            className="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            Previous
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            1
+          </a>
+        </li>
+        <li>
+          <a
+            onClick={next}
+            className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            Next
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
